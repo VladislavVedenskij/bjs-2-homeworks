9533d0e587e871fdc4c8f53a33f4e27f76d52fe4
Vladislav B <fashionbeatt@gmail.com>
2023-08-07 18:57:57 +0300

    -a

--- 8.decorators/task.js
+++ 8.decorators/task.js
@@ -1,9 +1,45 @@
 //Задача № 1
 function cachingDecoratorNew(func) {
-  
-}
+    const cache = [];
+    const maxCount = 5;
+    return function (...args) {
+        const hash = md5(args);
+        const index = cache.findIndex((element) => element.hash === hash);
+        if (index !== -1) {
+            return 'Из кэша: ' + cache[index].value;
+        }
+        if (cache.length === maxCount) {
+            cache.shift();
+        }
+        cache.push({ hash, value: func(...args) });
+        return 'Вычисляем: ' + cache[cache.length - 1].value;
+    };
+};
 
 //Задача № 2
 function debounceDecoratorNew(func, delay) {
-  
+    let timeoutId = null;
+    wrapper.allCount = 0;
+    wrapper.count = 0;
+    function countWrapper(...args) {
+        wrapper.count++;
+        func(...args);
+    }
+    function wrapper(...args) {
+        wrapper.allCount++;
+        if (wrapper.count !== 0) {
+            if (timeoutId) {
+                const tempId = timeoutId;
+                timeoutId = null;
+                clearTimeout(tempId);
+            }
+            timeoutId = setTimeout(() => {
+                timeoutId = null;
+                countWrapper(...args);
+            }, delay);
+        } else {
+            countWrapper(...args);
+        }
+    };
+    return wrapper;
 }
